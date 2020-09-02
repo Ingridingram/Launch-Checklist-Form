5e2c62514825fd6c19344bddb9edbf0dbcbf8b83
Ingrid Vacca <ingrid.ann.vacca@gmail.com>
2020-09-02 06:48:03 -0500

    started project:

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-// Write your JavaScript code here!
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let usernameInput = document.querySelector("input[name=pilotName]");
+      let copliotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass")
+      if (usernameInput.value === "" || copliotName.value === "" || isNaN(fuelLevel.value) === true || isNaN(cargoMass.value) === true) {
+         event.preventDefault()
+         alert("Invalid Input!")
+      }
+   });
+});
+
+
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
