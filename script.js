7a169d04ac1b0a9030f41f94fc8bb3305ea0562e
Ingrid Vacca <ingrid.ann.vacca@gmail.com>
2020-09-08 20:32:05 -0500

    Edited with Matt

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
+   let getPilotStatus = document.getElementById("pilotName")
    form.addEventListener("submit", function(event) {
-      let usernameInput = document.querySelector("input[name=pilotName]");
-      let copliotName = document.querySelector("input[name=copilotName]");
-      let fuelLevel = document.querySelector("input[name=fuelLevel]");
-      let cargoMass = document.querySelector("input[name=cargoMass")
-      if (usernameInput.value === "" || copliotName.value === "" || isNaN(fuelLevel.value) === true || isNaN(cargoMass.value) === true) {
          event.preventDefault()
+      let usernameInput = document.getElementById("pilotName").value;
+      // let copliotName = document.querySelector("input[name=copilotName]");
+      // let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      // let cargoMass = document.querySelector("input[name=cargoMass")
+      if (usernameInput.value === "") { 
+      // || copliotName.value === "" || isNaN(fuelLevel.value) === true || isNaN(cargoMass.value) === true) {
+         // event.preventDefault()
          alert("Invalid Input!")
+      } else {
+        getPilotStatus.innerHTML = `hello ${usernameInput}`
       }
    });
 });
 
 
-
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
+// /* This block of code shows how to format the HTML once you fetch some planetary JSON!
+{/* <h2>Mission Destination</h2>
 <ol>
    <li>Name: ${}</li>
    <li>Diameter: ${}</li>
@@ -24,5 +27,4 @@ window.addEventListener("load", function() {
    <li>Distance from Earth: ${}</li>
    <li>Number of Moons: ${}</li>
 </ol>
-<img src="${}">
-*/
+<img src="${}"> */}
